603410119b295acd92a40823f14b05ef4cffc109
janhvisingh965 <janhvisingh965@gmail.com>
2026-04-24 00:07:33 +0530

    Time: 167 ms (24.22%) | Memory: 153 MB (49.84%) - LeetSync

--- 1840-minimize-hamming-distance-after-swap-operations/minimize-hamming-distance-after-swap-operations.cpp
+++ 1840-minimize-hamming-distance-after-swap-operations/minimize-hamming-distance-after-swap-operations.cpp
@@ -8,35 +8,37 @@ public:
     }
 
     void unite(int a, int b) {
-        int pa = find(a);
-        int pb = find(b);
-        if (pa != pb) parent[pa] = pb;
+        a = find(a);
+        b = find(b);
+        if (a != b) parent[a] = b;
     }
 
     int minimumHammingDistance(vector<int>& source, vector<int>& target, vector<vector<int>>& allowedSwaps) {
         int n = source.size();
         parent.resize(n);
 
-        for (int i = 0; i < n; i++)
-            parent[i] = i;
+        for (int i = 0; i < n; i++) parent[i] = i;
 
-        for (auto &it : allowedSwaps)
-            unite(it[0], it[1]);
+        for (auto &x : allowedSwaps) {
+            unite(x[0], x[1]);
+        }
 
         unordered_map<int, vector<int>> groups;
 
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++) {
             groups[find(i)].push_back(i);
+        }
 
         int ans = 0;
 
-        for (auto &grp : groups) {
+        for (auto &g : groups) {
             unordered_map<int, int> freq;
 
-            for (int idx : grp.second)
+            for (int idx : g.second) {
                 freq[source[idx]]++;
+            }
 
-            for (int idx : grp.second) {
+            for (int idx : g.second) {
                 if (freq[target[idx]] > 0)
                     freq[target[idx]]--;
                 else
